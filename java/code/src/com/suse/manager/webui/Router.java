9996bc861ee38d7c87d6886880840811a24ea037
Johannes Hahn <johannes.hahn@suse.com>
2023-01-31 17:17:55 +0100

    Add view controller class, templates and link from menu

--- java/code/src/com/suse/manager/webui/Router.java
+++ java/code/src/com/suse/manager/webui/Router.java
@@ -67,6 +67,7 @@ import com.suse.manager.webui.controllers.channels.ChannelsApiController;
 import com.suse.manager.webui.controllers.contentmanagement.ContentManagementApiController;
 import com.suse.manager.webui.controllers.contentmanagement.ContentManagementViewsController;
 import com.suse.manager.webui.controllers.image.ImageManagementApiController;
+import com.suse.manager.webui.controllers.image.ImageManagementViewsController;
 import com.suse.manager.webui.controllers.login.LoginController;
 import com.suse.manager.webui.controllers.maintenance.MaintenanceCalendarController;
 import com.suse.manager.webui.controllers.maintenance.MaintenanceController;
@@ -263,6 +264,7 @@ public class Router implements SparkApplication {
         ImageStoreController.initRoutes(jade);
         ImageProfileController.initRoutes(jade);
         ImageBuildController.initRoutes(jade, imageBuildController);
+        ImageManagementViewsController.initRoutes(jade);
         ImageManagementApiController.initRoutes();
     }
 }
